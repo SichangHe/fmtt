538778e38de33ab2a331c820190c41f3ef6d5525
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-05-24 14:47:21 +0800

    basic python binding

---
+++ python/fmtt/__init__.py
@@ -0,0 +1,3 @@
+from fmtt._lowlevel import format
+
+__all__ = ["format"]
--- src/lib.rs
+++ src/lib.rs
@@ -27,5 +27,53 @@ pub fn format<'a>(
     result
 }
 
+#[cfg(feature = "py")]
+#[pyo3::pymodule]
+mod _lowlevel {
+    use pyo3::{exceptions::PyValueError, prelude::*};
+
+    use super::*;
+
+    /// Format text diff-friendly by breaking lines of sensible punctuations and
+    /// words.
+    ///
+    /// See <https://github.com/SichangHe/fmtt> for the options.
+    #[pyfunction]
+    #[pyo3(signature = (
+        text,
+        line_width=80,
+        allow_indented_paragraphs=false,
+        single_line_starts=vec![],
+        multi_line_starts=vec![],
+        ignore_line_starts=vec![],
+    ))]
+    fn format(
+        text: &str,
+        line_width: usize,
+        allow_indented_paragraphs: bool,
+        single_line_starts: Vec<String>,
+        multi_line_starts: Vec<String>,
+        ignore_line_starts: Vec<String>,
+    ) -> PyResult<String> {
+        let paragraph_starts = ParagraphStarts::try_from_str_slices(
+            &borrowed_str_slice(&single_line_starts),
+            &borrowed_str_slice(&multi_line_starts),
+            &borrowed_str_slice(&ignore_line_starts),
+        )
+        .map_err(|why| PyValueError::new_err(format!("{why}")))?;
+        let formatted_words = super::format(
+            text,
+            line_width,
+            allow_indented_paragraphs,
+            &paragraph_starts,
+        );
+        Ok(formatted_words.join(""))
+    }
+
+    fn borrowed_str_slice(slice: &[String]) -> Vec<&str> {
+        slice.iter().map(String::as_str).collect()
+    }
+}
+
 #[cfg(test)]
 mod tests;
